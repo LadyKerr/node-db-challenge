f85f6676351b34caafcf9b9eac1fda05e641c613
LadyKerr <kadiekerr@gmail.com>
2019-07-19 12:50:43 -0400

    MVP complete

--- project-tracker/actions/action-router.js
+++ project-tracker/actions/action-router.js
@@ -4,25 +4,5 @@ const Actions = require("./action-model");
 
 const router = express.Router();
 
-//add new action for existing project
-//since actions have a relationship to projects, projects_id must be included
-router.post("/:id/actions", (req, res) => {
-  let newAction = req.body;
-  const { description, notes } = req.params;
-  const { id } = req.params;
-  newAction.project_id = id;
-
-  if (!description || !notes) {
-    res.status(400).json({ errorMessage: "Please add notes and description." });
-  } else {
-    Actions.add(req.body)
-      .then(newAction => {
-        res.status(200).json(newAction);
-      })
-      .catch(err => {
-        res.status(500).json({ message: "There was an error adding action" });
-      });
-  }
-});
 
 module.exports = router;
--- project-tracker/projects/project-router.js
+++ project-tracker/projects/project-router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const Projects = require("./project-model");
+const Actions = require("../actions/action-model");
 
 const router = express.Router();
 
@@ -40,7 +41,7 @@ router.get("/:id", (req, res) => {
 
 //add new project
 router.post("/", (req, res) => {
-  const { name, description } = req.params;
+  const { name, description } = req.body;
 
   if (!name || !description) {
     res.status(400).json({ errorMessage: "Please add name and description." });
@@ -50,7 +51,30 @@ router.post("/", (req, res) => {
         res.status(200).json(newProject);
       })
       .catch(err => {
+        console.log(err);
         res.status(500).json({ message: "There was an error adding project" });
+      });
+  }
+});
+
+//add new action for existing project
+//since actions have a relationship to projects, projects_id must be included
+router.post("/:id/actions", (req, res) => {
+  let newAction = req.body;
+  const { description, notes } = req.body;
+  const { id } = req.params;
+  newAction.project_id = id;
+
+  if (!description || !notes) {
+    res.status(400).json({ errorMessage: "Please add notes and description." });
+  } else {
+    Actions.add(req.body)
+      .then(newAction => {
+        res.status(200).json(newAction);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: "There was an error adding action" });
       });
   }
 });
