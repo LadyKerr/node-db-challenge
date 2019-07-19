96f9fca8362288160e8df3afee59cb079360cc5f
LadyKerr <kadiekerr@gmail.com>
2019-07-19 12:30:22 -0400

    action router

--- project-tracker/actions/action-router.js
+++ project-tracker/actions/action-router.js
@@ -1,7 +1,28 @@
 const express = require("express");
 
-// const Actions = require("./action-model");
+const Actions = require("./action-model");
 
 const router = express.Router();
+
+//add new action for existing project
+//since actions have a relationship to projects, projects_id must be included
+router.post("/:id/actions", (req, res) => {
+  let newAction = req.body;
+  const { description, notes } = req.params;
+  const { id } = req.params;
+  newAction.project_id = id;
+
+  if (!description || !notes) {
+    res.status(400).json({ errorMessage: "Please add notes and description." });
+  } else {
+    Actions.add(req.body)
+      .then(newAction => {
+        res.status(200).json(newAction);
+      })
+      .catch(err => {
+        res.status(500).json({ message: "There was an error adding action" });
+      });
+  }
+});
 
 module.exports = router;
